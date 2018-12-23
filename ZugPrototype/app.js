3c7d98558e85a3584300084da3040a33e8f07557
HunterZ1994 <martin.thuerauf.1994@gmail.com>
2018-12-23 21:28:51 +0100

    nodemodules eingefügt. Ebenso logging module

--- ZugPrototype/app.js
+++ ZugPrototype/app.js
@@ -1,6 +1,11 @@
 //requirements
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const fs = require("fs");
+
+//logging
+app.use(morgan('common', {stream: fs.createWriteStream('./requests.log', {flags: 'a'})}));
 
 //Routing handler
 const init = require("./api/init");
