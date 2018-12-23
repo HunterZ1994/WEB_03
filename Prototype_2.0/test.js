2dc9cc553ac47d8ee0c8c880cc688f58ba9785ce
Bianca <namuu@web.de>
2018-12-23 21:43:29 +0100

    Removed Test files and button

--- Prototype_2.0/test.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res, next)=>{
-    var id = req.query.hiddenID;
-    res.send("testfile called" + id);
-});
-
-module.exports = router;
